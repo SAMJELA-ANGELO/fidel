29e2eced673a9dd54722c1ba54c7bd0b244ee87f
SAMJELA ANGELO <samjelaangelo@gmail.com>
2025-08-09 01:16:30 +0100

    inital commit

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
